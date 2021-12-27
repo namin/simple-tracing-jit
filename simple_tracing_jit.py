c3ffb72e9a0e77a60f33b9c495eaa7394a1c0b98
Nada Amin <namin@seas.harvard.edu>
2021-12-27 13:51:54 +0100

    upgrade to Python 3

--- simple_tracing_jit.py
+++ simple_tracing_jit.py
@@ -46,28 +46,28 @@ class Interpreter(object):
         self.code = code
 
     def run_PUSH(self):
-        #print "Running PUSH"
+        #print("Running PUSH")
         self.stack.append(self.code[self.pc+1])
         self.pc += 2
 
     def run_GT(self):
-        #print "Running GT"
+        #print("Running GT")
         if self.stack[-1] > self.code[self.pc+1]:
             self.pc = self.code[self.pc+2]
         else:
             self.pc += 3
 
     def run_ADD(self):
-        #print "Running ADD"
+        #print("Running ADD")
         self.stack[-1] += self.code[self.pc+1]
         self.pc += 2
 
     def run_JUMP(self):
-        #print "Running JUMP"
+        #print("Running JUMP")
         self.pc = self.code[self.pc+1]
 
     def run_POP(self):
-        #print "Running POP"
+        #print("Running POP")
         self.stack.pop()
         self.pc += 1
 
@@ -150,13 +150,13 @@ def trace_%d():
         return executable_trace
 
     def print_state(self):
-        print "State is pc =", self.pc, "stack =", self.stack
-        #print "JITted scope =", self.jitted_code_scope
+        print("State is pc =", self.pc, "stack =", self.stack)
+        #print("JITted scope =", self.jitted_code_scope)
 
     def enter_trace(self, loop_info):
-        #print loop_info['executable_trace']
-        exec loop_info['executable_trace'] in self.jitted_code_scope # defines the trace in the jitted context
-        exec 'trace_%d()' % (loop_info['trace_id']) in self.jitted_code_scope
+        #print(loop_info['executable_trace'])
+        exec(loop_info['executable_trace'], self.jitted_code_scope) # defines the trace in the jitted context
+        exec('trace_%d()' % (loop_info['trace_id']), self.jitted_code_scope)
 
     def run_JUMP(self):
         old_pc = self.pc
@@ -170,38 +170,38 @@ def trace_%d():
 
                 if loop_info['has_trace']:
                     Interpreter.run_JUMP(self) # run the jump, then run the trace
-                    #print "Running previously-compiled trace for loop", new_pc, "-",  old_pc
-                    print "Starting trace", new_pc, '-', old_pc
+                    #print("Running previously-compiled trace for loop", new_pc, "-",  old_pc)
+                    print("Starting trace", new_pc, '-', old_pc)
                     self.print_state()
                     try:
                         self.enter_trace(loop_info)
                         # can a trace leave normally? no, it is an infinite loop
                     except GuardFailed:
-                        print "Guard failed after", self.count, "iterations, leaving trace for interpreter execution"
+                        print("Guard failed after", self.count, "iterations, leaving trace for interpreter execution")
                         self.print_state()
                         return # Trace execution was not good for this iteration, so, fallback to regular interpreter
                                # the jitted code is modifying interpreter state, no need to sync
 
                 if loop_info['hotness'] > 10 and loop_info['has_trace'] == False:
                     if not self.recording_trace:
-                        print "Found new hot loop from", new_pc, "to", old_pc, "(included)"
+                        print("Found new hot loop from", new_pc, "to", old_pc, "(included)")
                         self.recording_trace = True
 
                         Interpreter.run_JUMP(self) # run the jump normally so that we start the trace at the beginning of the loop
                         recording_interpreter = RecordingInterpreter(self.pc, self.stack, self.code, self.loops, self.recording_trace, old_pc)
                         try:
-                            print "Trace recording started at pc =", new_pc, "until (included) pc =", old_pc
+                            print("Trace recording started at pc =", new_pc, "until (included) pc =", old_pc)
                             recording_interpreter.interpret()
                         except TraceRecordingEnded:
-                            print "Trace recording ended!"
+                            print("Trace recording ended!")
                             self.pc = recording_interpreter.pc # the rest are mutable datastructures that were shared with the recording interp
                             self.recording_trace = False
 
                             loop_info['trace_id'], loop_info['trace'] = self.trace_id, recording_interpreter.trace
                             self.trace_id += 1
                             loop_info['has_trace'], loop_info['executable_trace'] =  True, self.translate_trace(loop_info)
-                            print "Execution trace:", loop_info['executable_trace']
-                            print "Now jumping into compiled trace!"
+                            print("Execution trace:", loop_info['executable_trace'])
+                            print("Now jumping into compiled trace!")
                             TracingInterpreter.run_JUMP(self) # recursive call, but this time it will run the compiled trace
                             return
 
@@ -230,17 +230,17 @@ class RecordingInterpreter(TracingInterpreter):
         return current_pc == self.end_of_trace
 
     def run_PUSH(self):
-        #print "Recording PUSH"
+        #print("Recording PUSH")
         self.trace.append( (TRACE_INSTR, self.code[self.pc], self.code[self.pc+1]) )
         TracingInterpreter.run_PUSH(self)
 
     def run_ADD(self):
-        #print "Recording ADD"
+        #print("Recording ADD")
         self.trace.append( (TRACE_INSTR, self.code[self.pc], self.code[self.pc+1]) )
         TracingInterpreter.run_ADD(self)
 
     def run_GT(self):
-        #print "Recording GT"
+        #print("Recording GT")
 
         if self.stack[-1] > code[self.pc+1]:
             self.trace.append( (TRACE_GUARD_GT_JUMP, code[self.pc+1]) )
@@ -253,7 +253,7 @@ class RecordingInterpreter(TracingInterpreter):
 
     def run_JUMP(self):
         end_of_trace = self.is_end_of_trace(self.pc)
-        #print "Recording JUMP"
+        #print("Recording JUMP")
         self.trace.append( (TRACE_INSTR, self.code[self.pc], self.code[self.pc+1]) )
         if end_of_trace:
             raise TraceRecordingEnded()
@@ -261,7 +261,7 @@ class RecordingInterpreter(TracingInterpreter):
         TracingInterpreter.run_JUMP(self)
 
     def run_POP(self):
-        #print "Recording POP"
+        #print("Recording POP")
         self.trace.append( (TRACE_INSTR, POP))
         TracingInterpreter.run_POP(self)
 
@@ -270,5 +270,5 @@ class RecordingInterpreter(TracingInterpreter):
         TracingInterpreter.enter_trace(self, loop_info)
 
 
-print TracingInterpreter(0, [], code, {}, False).interpret()
-print Interpreter(0, [], code).interpret()
+print(TracingInterpreter(0, [], code, {}, False).interpret())
+print(Interpreter(0, [], code).interpret())
