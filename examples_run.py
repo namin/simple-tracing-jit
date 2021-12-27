a2c77f5ecfd9c072203b4c355ace79a02803f0c0
Nada Amin <namin@seas.harvard.edu>
2021-12-27 22:35:09 +0100

    fix assertions

--- examples_run.py
+++ examples_run.py
@@ -5,6 +5,6 @@ import simple_tracing_jit
 for (title, code, expected) in examples:
     print("# Example", title)
     res1 = simple_interpreter.interpret(code)
-    assert expected == res1, "in example %s, expected %d, got %d" % (title, expected, res)
+    assert expected == res1, "in example %s, expected %d, got %d" % (title, expected, res1)
     res2 = simple_tracing_jit.interpret(code)
-    assert expected == res2, "in example %s, expected %d, got %d" % (title, expected, res)
+    assert expected == res2, "in example %s, expected %d, got %d" % (title, expected, res2)
