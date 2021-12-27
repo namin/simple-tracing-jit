ae973b6437850c1c636db2b4465e5da7427e88d7
Nada Amin <namin@seas.harvard.edu>
2021-12-27 21:45:41 +0100

    separate examples, and fix some small revealed issues

---
+++ examples.py
@@ -0,0 +1,106 @@
+from simple_tracing_jit import *
+
+one_simple_loop = [
+    PUSH, 0,     # 0
+    GT, 50000, 9, # 2
+    ADD, 1,       # 5
+    JUMP, 2,      # 7
+    HALT         # 9
+]
+
+one_simple_loop1 = [
+    PUSH, 0,     # 0
+    GT, 10, 9, # 2
+    ADD, 1,       # 5
+    JUMP, 2,      # 7
+    HALT         # 9
+]
+
+two_simple_loops = [
+    PUSH, 0,     # 0
+    GT, 50000, 9, # 2
+    ADD, 1,       # 5
+    JUMP, 2,      # 7
+
+    GT, 100000, 16, # 9
+    ADD, 2,         # 12
+    JUMP, 9,        # 14
+
+    HALT         # 16
+]
+
+nested_loops = [
+    PUSH, 0,     # 0 - outer loop counter
+    GT, 30, 19, # 2 - start of outer loop
+    PUSH, 0,     # 5 - inner loop counter
+    GT, 15, 14,  # 7 - start of inner loop
+    ADD, 1,      # 10
+    JUMP, 7,     # 12 - end of inner loop
+    POP,         # 14
+    ADD, 2,      # 15
+    JUMP, 2,      # 17 - end of outer loop
+
+    HALT         # 19
+]
+
+nested_loops1 = [
+    PUSH, 0,     # 0 - outer loop counter
+    GT, 30, 19, # 2 - start of outer loop
+    PUSH, 0,     # 5 - inner loop counter
+    GT, 10, 14,  # 7 - start of inner loop
+    ADD, 1,      # 10
+    JUMP, 7,     # 12 - end of inner loop
+    POP,         # 14
+    ADD, 2,      # 15
+    JUMP, 2,      # 17 - end of outer loop
+
+    HALT         # 19
+]
+
+nested_loops2 = [
+    PUSH, 0,     # 0 - outer loop counter
+    GT, 70, 22, # 2 - start of outer loop
+    GT, 24, 18, # +3
+    PUSH, 0,     # 5+3 - inner loop counter
+    GT, 15, 17,  # 7+3 - start of inner loop
+    ADD, 1,      # 10+3
+    JUMP, 10,     # 12+3 - end of inner loop
+    POP,         # 14+3
+    ADD, 2,      # 15+3
+    JUMP, 2,      # 17+3 - end of outer loop
+
+    HALT         # 19+3
+]
+
+nested_double_loops = [
+    PUSH, 0,   # 0
+    GT, 20, 29,# 2
+    PUSH, 0,   # 5
+    GT, 15, 14,# 7
+    ADD, 1,    # 10
+    JUMP, 7,   # 12
+    POP,       # 14
+    PUSH, 0,   # 15
+    GT, 20, 24,# 17
+    ADD, 1,    # 20
+    JUMP, 17,  # 22
+    POP,       # 24
+    ADD, 1,    # 25
+    JUMP, 2,   # 27
+    HALT       # 29
+]
+
+examples = [
+    ("1", one_simple_loop, 50001),
+    ("1a", one_simple_loop1, 11),
+    ("2", two_simple_loops, 100001),
+    ("3", nested_loops, 32),
+    ("3a", nested_loops1, 32),
+    ("3b", nested_loops2, 72),
+    ("4", nested_double_loops, 21)
+]
+
+for (title, code, expected) in examples:
+    print("# Example", title)
+    res = interpret(code)
+    assert expected == res, "in example %s, expected %d, got %d" % (title, expected, res)
--- simple_tracing_jit.py
+++ simple_tracing_jit.py
@@ -1,103 +1,5 @@
 PUSH, ADD, JUMP, GT, HALT, POP = range(6)
 
-one_simple_loop = [
-    PUSH, 0,     # 0
-    GT, 50000, 9, # 2
-    ADD, 1,       # 5
-    JUMP, 2,      # 7
-    HALT         # 9
-]
-
-one_simple_loop1 = [
-    PUSH, 0,     # 0
-    GT, 10, 9, # 2
-    ADD, 1,       # 5
-    JUMP, 2,      # 7
-    HALT         # 9
-]
-
-two_simple_loops = [
-    PUSH, 0,     # 0
-    GT, 50000, 9, # 2
-    ADD, 1,       # 5
-    JUMP, 2,      # 7
-
-    GT, 100000, 16, # 9
-    ADD, 2,         # 12
-    JUMP, 9,        # 14
-
-    HALT         # 16
-]
-
-nested_loops = [
-    PUSH, 0,     # 0 - outer loop counter
-    GT, 30, 19, # 2 - start of outer loop
-    PUSH, 0,     # 5 - inner loop counter
-    GT, 15, 14,  # 7 - start of inner loop
-    ADD, 1,      # 10
-    JUMP, 7,     # 12 - end of inner loop
-    POP,         # 14
-    ADD, 2,      # 15
-    JUMP, 2,      # 17 - end of outer loop
-
-    HALT         # 19
-]
-
-nested_loops1 = [
-    PUSH, 0,     # 0 - outer loop counter
-    GT, 30, 19, # 2 - start of outer loop
-    PUSH, 0,     # 5 - inner loop counter
-    GT, 10, 14,  # 7 - start of inner loop
-    ADD, 1,      # 10
-    JUMP, 7,     # 12 - end of inner loop
-    POP,         # 14
-    ADD, 2,      # 15
-    JUMP, 2,      # 17 - end of outer loop
-
-    HALT         # 19
-]
-
-nested_loops2 = [
-    PUSH, 0,     # 0 - outer loop counter
-    GT, 70, 22, # 2 - start of outer loop
-    GT, 24, 18, # +3
-    PUSH, 0,     # 5+3 - inner loop counter
-    GT, 15, 17,  # 7+3 - start of inner loop
-    ADD, 1,      # 10+3
-    JUMP, 10,     # 12+3 - end of inner loop
-    POP,         # 14+3
-    ADD, 2,      # 15+3
-    JUMP, 2,      # 17+3 - end of outer loop
-
-    HALT         # 19+3
-]
-
-nested_double_loops = [
-    PUSH, 0,   # 0
-    GT, 20, 29,# 2
-    PUSH, 0,   # 5
-    GT, 15, 14,# 7
-    ADD, 1,    # 10
-    JUMP, 7,   # 12
-    POP,       # 14
-    PUSH, 0,   # 15
-    GT, 20, 24,# 17
-    ADD, 1,    # 20
-    JUMP, 17,  # 22
-    POP,       # 24
-    ADD, 1,    # 25
-    JUMP, 2,   # 27
-    HALT       # 29
-]
-
-code = one_simple_loop
-code = one_simple_loop1
-code = two_simple_loops
-code = nested_loops
-code = nested_loops1
-code = nested_loops2
-code = nested_double_loops
-
 class Interpreter(object):
     def __init__(self, pc, stack, code):
         self.pc = pc
@@ -169,7 +71,7 @@ def trace_{id}():
     count = 0
     while True:
         count += 1
-        print('in trace_{id}, iteration '+str(count))
+        #print('in trace_{id}, iteration '+str(count))
 '''.format_map({'id': loop_info['trace_id']})
 
         for trace_step in trace:
@@ -305,11 +207,11 @@ class RecordingInterpreter(TracingInterpreter):
     def run_GT(self):
         #print("Recording GT")
 
-        if self.stack[-1] > code[self.pc+1]:
-            self.trace.append( (TRACE_GUARD_GT_JUMP, code[self.pc+1]) )
-            self.trace.append( (TRACE_INSTR, JUMP, code[self.pc+2]) )
+        if self.stack[-1] > self.code[self.pc+1]:
+            self.trace.append( (TRACE_GUARD_GT_JUMP, self.code[self.pc+1]) )
+            self.trace.append( (TRACE_INSTR, JUMP, self.code[self.pc+2]) )
         else:
-            self.trace.append( (TRACE_GUARD_GT_NOT_JUMP, code[self.pc+1]) )
+            self.trace.append( (TRACE_GUARD_GT_NOT_JUMP, self.code[self.pc+1]) )
             self.trace.append( (TRACE_INSTR, JUMP, self.pc+3) )
 
         TracingInterpreter.run_GT(self)
@@ -334,5 +236,10 @@ class RecordingInterpreter(TracingInterpreter):
         TracingInterpreter.enter_trace(self, loop_info)
 
 
-print(TracingInterpreter(0, [], code, {}, False).interpret())
-print(Interpreter(0, [], code).interpret())
+def interpret(code):
+    print("## Interpreter")
+    res1 = Interpreter(0, [], code).interpret()
+    print("## Tracing Interpreter")
+    res2 = TracingInterpreter(0, [], code, {}, False).interpret()
+    assert(res1==res2)
+    return res1
